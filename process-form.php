08ffddb59e91f8e55b74782b71b6a803df30a994
Sagar Jha <sagarjha1311@gmail.com>
2023-12-25 18:32:33 +0530

    contact form

--- process-form.php
+++ process-form.php
@@ -1,42 +1,49 @@
-
-
-
-
 <?php
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
-
-require "vendor/autoload.php";
-
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer(true);
-
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-
-$mail->Host = "smtp.example.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = "surya.rawcleaners@gmail.com";
-$mail->Password = "fyhf ggjr jiye ymsc";
-
-$mail->setFrom($email, $name);
-$mail->addAddress("surya.rawcleaners@gmail.com", "Surya");
-
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: sent.html");
+require 'vendor/autoload.php'; // Path to autoload.php of PHPMailer
 
 
-// fyhf ggjr jiye ymsc
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // PHPMailer configuration
+    $mail = new PHPMailer(true);
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Your SMTP host
+        $mail->SMTPAuth = true;
+        $mail->Username = 'surya.rawcleaners@gmail.com'; // SMTP username
+        $mail->Password = 'mtru meps pqjn uhhm'; // SMTP password
+        // $mail->SMTPSecure = 'tls';
+        $mail->Port = 587; // Port number
+
+        // Sender and recipient settings
+        $mail->setFrom($email, $name);
+        $mail->addAddress('vasanth2640@gmail.com'); // Receiver email
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
+
+        // Send email
+        if ($mail->send()) {
+            // Mail sent successfully, redirect or display a success message
+            header('Location: index.html'); // Redirect to your landing page
+            exit();
+        } else {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
